2482f9bd146aa2ddb09d2bc9ec9b7db97c9e792a
Siddarth Mewara <siddarth.mewara597@gmail.com>
2023-03-18 01:17:45 +0530

    add routes on client & third party cdn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/product/:handle" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
